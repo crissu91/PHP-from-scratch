81eab310c565511518ed1ae2b10d9c3c508a0ee2
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-02-16 10:36:06 +0000

    associative arrays

--- 02-arrays-and-iteration/03-associative-arrays/index.php
+++ 02-arrays-and-iteration/03-associative-arrays/index.php
@@ -1,5 +1,21 @@
 <?php
 $output = null;
+
+$user = [
+    'name' => 'John',
+    'email' => 'example@gmail.com',
+    'password' => '123456',
+    'hobbies' => ['tennis', 'games'],
+];
+
+$output = $user['name'];
+$output = $user['email'];
+
+$output = $user['hobbies'][0];
+
+$user['address'] = ' 123 MAin Street';
+
+unset($user['address']);
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +38,12 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4"> User array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($user); ?>
+                </pre>
+            </p>
         </div>
     </div>
 </body>
