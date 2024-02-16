39e01d41126ceb8b6ab32fff44afd063b7a687ec
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-02-16 14:58:25 +0000

    if statements

--- 03-control-structures/01-if-statements/index.php
+++ 03-control-structures/01-if-statements/index.php
@@ -1,10 +1,38 @@
 <?php
 
+$age = 16;
+
 // If statement
 
+// if ($age >= 21) {
+//     echo ' you are old enought to drink in US';
+// }
 // If-Else
+
+// if ($age >= 21) {
+//     echo ' you are old enought to drink in US';
+// } else {
+//     echo ' you are NOT old enought to drink in US';
+// }
 
 // Nested if statement
 
+// if ($age >= 21) {
+//     echo ' you are old enought to drink and vote in US';
+// } else {
+//     if ($age >= 18) {
+//         echo ' you are NOT old enought to vote in US';
+//     } else {
+//         echo ' you are NOT old enought to drink nor to buy alcohol in US';
+//     }
+// }
+
 // If-Else-If
 
+if ($age >= 21) {
+    echo ' you are old enought to drink and vote in US';
+} else if ($age >= 18) {
+    echo ' you are NOT old enought to vote in US';
+} else {
+    echo ' you are NOT old enought to drink nor to buy alcohol in US';
+}
