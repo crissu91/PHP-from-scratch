5a9f8a0f319ba42e8899c4a41942dde3b8cf4120
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-02-20 11:27:12 +0000

    anonymous functions and closure

---
+++ 04-functions/08-anonymous-functions-closures/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$square = function ($number) {
+    return $number * $number;
+};
+
+$result = $square(5);
+
+echo 'The square of 5 is ' . $result . '<br/>';
+
+//Closure
+
+function createCounter()
+{
+    $count = 0;
+
+    $counter = function () use (&$count) {
+        return ++$count;
+    };
+
+    return $counter;
+}
+
+$counter = createCounter();
+
+echo $counter() . '<br/>';
