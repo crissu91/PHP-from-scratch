6602b01dc333d2b12ab0c8a4fde92a6f8bf32c71
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-02-23 15:25:41 +0000

    abstract classes and methods

---
+++ 05-oop/06-abstract-classes-methods/index.php
@@ -0,0 +1,61 @@
+<?php
+
+abstract class Shape
+{
+    protected $name;
+
+    abstract public function calculateArea();
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    //Concrete method
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+class Circle extends Shape
+{
+    private $radius;
+
+    public function __construct($name, $radius)
+    {
+        parent::__construct($name);
+        $this->radius = $radius;
+    }
+
+    public function calculateArea()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+class Rectangle extends Shape
+{
+    private $width;
+    private $height;
+
+    public function __construct($name, $width, $height)
+    {
+        parent::__construct($name);
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function calculateArea()
+    {
+        return $this->width * $this->height;
+    }
+}
+
+$circle = new Circle('Circle', 5);
+$rectangle = new Rectangle('Rectangle', 4, 6);
+
+// var_dump($rectangle);
+
+echo $circle->getName() . ' Area: ' . $circle->calculateArea() . '<br/>';
+
+echo $rectangle->getName() . ' Area: ' . $rectangle->calculateArea() . '<br/>';
