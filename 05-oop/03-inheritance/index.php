ddc7e2aa8a0bdf19c317423dc324094a1772e4d7
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-02-23 14:45:19 +0000

    inheritance

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,34 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User
+{
+  public $level;
+
+  public function __construct($name, $email, $level)
+  {
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+
+  public function login()
+  {
+    echo 'Admin ' . $this->name . ' logged in.';
+  }
+
+  public function getstatus()
+  {
+    echo $this->status;
+  }
+}
+
+$admin1 = new Admin('Tom Smith', 'tom@gmail.com', 5);
+
+// echo $admin1->name;
+// echo $admin1->email;
+// echo $admin1->level;
+$admin1->login();
+// $admin1->getStatus();
+
+// var_dump($admin1);
