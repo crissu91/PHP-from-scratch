0849e4f2ec115c0628003af2ba63558a09502d7d
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-03-08 16:49:50 +0000

    select single record

--- 07-database/10-insert-record/create.php
+++ 07-database/10-insert-record/create.php
@@ -1,3 +1,27 @@
+<?php
+
+require 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+  $title = htmlspecialchars($_POST['title']);
+  $body = htmlspecialchars($_POST['body']);
+
+  $sql = 'ISERT INTO posts (title, body) VALUES (:title, :body)';
+
+  $stmt = $pdo->prepare($sql);
+
+  $params = [
+    'title' => $title,
+    'body' => $body
+  ];
+
+  $stmt->execute($params);
+
+  header('Location: index.php');
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
