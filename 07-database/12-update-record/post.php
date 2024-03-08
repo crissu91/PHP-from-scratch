9e443fa95f1b658cc1942f7a43182160f8d017bc
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-03-08 16:50:19 +0000

    update records

--- 07-database/12-update-record/post.php
+++ 07-database/12-update-record/post.php
@@ -51,6 +51,9 @@ $post = $stmt->fetch();
           <a href="index.php">Go Back</a>
         </div>
       </div>
+
+      <a href="edit.php?id-<?= $post['id'] ?>" class="bg-green-500 text-white px-4 py-2 rounded block w-full text-center mb-4 hover:bg-green-600 focus:outline-none">Edit</a>
+
       <form action="delete.php" method="post">
         <input type="hidden" name="_method" value="delete">
         <input type="hidden" name="id" value="<?= $post['id'] ?>">
